2ee5e6f3aac4b5909632982ff8047f778b13d317
Anthony <anthonykimlao@gmail.com>
2023-12-28 20:58:43 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name){
+  const filteredNames = drivers.filter((newName)=>newName.toLowerCase()===(name.toLowerCase()));
+  return filteredNames;
+}
+
+function fuzzyMatch(drivers, letter){
+  const filteredNames = drivers.filter((newName)=>newName.startsWith(letter));
+  return filteredNames;
+}
+
+function matchName(drivers, home){
+  const nameMatch = drivers.filter((newHome)=>newHome.name === home)
+  return nameMatch;
+}
